5f8d2e3d03fb1850c94ef7f9706fe199301eb3aa
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-29 23:05:47 +0100

    reading flashcards

--- src/pages/PageRandomOrder.tsx
+++ src/pages/PageRandomOrder.tsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
+import { AppContext } from '../AppContext';
+
 export const PageRandomOrder = () => {
+	const { flashcards } = useContext(AppContext);
+
 	return (
 		<div className="page pageRandomOrder">
-			<p>This is the RandomOrder page.</p>
+			<p>There are {flashcards.length} flashcards.</p>
 		</div>
 	);
 };
